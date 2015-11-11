66d6a4bfea5743939817f6889e317fbd7b30330c
Ainur Minibaev <rapper01@list.ru>
2015-11-11 12:33:44 +0300

    add new channel

--- dev/js/data_subscribs.js
+++ dev/js/data_subscribs.js
@@ -1,30 +1,35 @@
-﻿if(!_data){
+﻿if (!_data) {
     var _data = {};
 }
 _data['subscribs'] = {
-    'dI1':{
-        "uid":'863013',
-        'script_url_1_2':'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_2',
-        'did[]':863008
+    'dI1': {
+        "uid": '700107',
+        'script_url_1_61': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_61',
+        'did[]': 863008
     },
-    'dI2':{
-        "uid":'700107',
-        'script_url_1_3':'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_3',
-        'did[]':863010
+    'dI2': {
+        "uid": '700107',
+        'script_url_1_3': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_3',
+        'did[]': 863010
     },
-    'dI3':{
-        "uid":'700107',
-        'script_url_1_81':'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_81',
-        'did[]':863011
+    'dI3': {
+        "uid": '700107',
+        'script_url_1_81': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_81',
+        'did[]': 863011
     },
-    'dI4':{
-        "uid":'700107',
-        'script_url_1_51':'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_51',
-        'did[]':863013
+    'dI4': {
+        "uid": '700107',
+        'script_url_1_51': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_51',
+        'did[]': 863013
     },
-    'default':{
-        "uid":'700107',
-        'script_url_1_98':'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_98',
-        'did[]':863014
+    'dI5': {
+        "uid": '700107',
+        'script_url_1_83': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_83',
+        'did[]': 870441
+    },
+    'default': {
+        "uid": '700107',
+        'script_url_1_98': 'https://imgs.smartresponder.ru/on/2660f41fa63a21f8c32d7ea4c07c40d997923909/1_98',
+        'did[]': 863014
     }
 };
